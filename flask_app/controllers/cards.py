682eebb8e27471f7c6cef2b195ddc3c695f72736
Tiffany O <tiffanyortega29@gmail.com>
2023-02-28 00:15:07 -0600

    fixed routes and functionatlity for update card

--- flask_app/controllers/cards.py
+++ flask_app/controllers/cards.py
@@ -79,7 +79,7 @@ def add_card_to_db():
 def edit_card_in_db(id):
     if "user_id" not in session:
         return redirect("/")
-    if not Card.validate_card(request.form):
+    if not card.Card.validate_card(request.form):
         return redirect(f"/edit/{id}")
 
     data = {
