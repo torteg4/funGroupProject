9d376c0c80293f198372a7f0207c7e17a601ab09
Ubaeby <christianchoi45@gmail.com>
2023-02-28 16:53:01 -0800

    Added cards into the dashboard

--- flask_app/controllers/cards.py
+++ flask_app/controllers/cards.py
@@ -9,7 +9,7 @@ def all_cards_page():
     data = {
         "id": session["user_id"]
     }
-    return render_template("dashboard.html", this_user= user.User.get_user_by_id(data))
+    return render_template("dashboard.html", this_user= user.User.get_user_by_id(data), cards = card.Card.get_all_cards())
 
 
 # route that will show the new card page
